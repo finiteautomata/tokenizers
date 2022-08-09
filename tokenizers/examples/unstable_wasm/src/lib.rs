e3052541e4519480bc68c814ddc818602b92b8b4
Juan Manuel Pérez <jmperez.85@gmail.com>
2022-08-09 14:34:31 -0300

    Add wasm tokenizers with some methods

--- tokenizers/examples/unstable_wasm/src/lib.rs
+++ tokenizers/examples/unstable_wasm/src/lib.rs
@@ -1,44 +1,67 @@
 mod utils;
-use tokenizers::models::bpe::{Vocab, BPE};
-use tokenizers::Tokenizer;
-
+use tokenizers::tokenizer::{Tokenizer, Encoding};
 use wasm_bindgen::prelude::*;
 
+use std::str::FromStr;
+use js_sys;
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+
 #[wasm_bindgen]
-pub fn tokenize(string: &str) -> Vec<u32> {
-    let vocab: Vocab = vec![
-        ("a".to_string(), 0),
-        ("##b".to_string(), 1),
-        ("##c".to_string(), 2),
-        ("ab".to_string(), 3),
-        ("abc".to_string(), 4),
-    ]
-    .into_iter()
-    .collect();
+pub struct TokenizerWasm {
+    tokenizer: Tokenizer
+}
 
-    let merges = vec![
-        ("a".to_string(), "##b".to_string()),
-        ("ab".to_string(), "##c".to_string()),
-    ];
+#[wasm_bindgen]
+impl TokenizerWasm {
 
-    let bpe = BPE::builder()
-        .vocab_and_merges(vocab, merges)
-        .unk_token("[UNK]".to_string())
-        .continuing_subword_prefix("##".to_string())
-        .build()
-        .unwrap();
-    let tokenizer = Tokenizer::new(bpe);
-    tokenizer
-        .encode(string, false)
-        .unwrap()
-        .get_ids()
-        .into_iter()
-        .cloned()
-        .collect()
+    #[wasm_bindgen(constructor)]
+    pub fn from_buffer(json: String) -> TokenizerWasm {
+        TokenizerWasm { tokenizer: Tokenizer::from_str(json.as_str()).unwrap().into() }
+    }
+
+    pub fn encode(&self, text: &str, add_special_tokens: bool) -> EncodingWasm {
+        EncodingWasm {encoding : self.tokenizer.encode(text, add_special_tokens).unwrap() }
+    }
+}
+
+#[wasm_bindgen]
+pub struct EncodingWasm {
+    encoding: Encoding
+}
+
+#[wasm_bindgen]
+impl EncodingWasm {
+
+    #[wasm_bindgen(method, getter = input_ids)]
+    pub fn get_ids(&self) -> js_sys::Uint32Array {
+        self.encoding.get_ids().into()
+    }
+
+
+    #[wasm_bindgen(method, getter = attention_mask)]
+    pub fn get_attention_mask(&self) -> js_sys::Uint32Array {
+        self.encoding.get_attention_mask().into()
+    }
+
+    #[wasm_bindgen(method, getter = tokens)]
+    pub fn get_tokens(&self) -> js_sys::Array {
+        self.encoding.get_tokens().iter().map(|x|{
+            js_sys::JsString::from(x.as_str())
+        }).collect()
+    }
+
+    #[wasm_bindgen(method, getter = word_ids)]
+    pub fn get_word_ids(&self) -> js_sys::Array {
+        self.encoding.get_word_ids().iter().map(|x| {
+            match x {
+                Some(y) => js_sys::Number::from(*y),
+                None => js_sys::Number::from(-1)
+            }
+        }).collect()
+    }
 }
--- tokenizers/examples/unstable_wasm/www/index.js
+++ tokenizers/examples/unstable_wasm/www/index.js
@@ -1,4 +1,27 @@
-import * as wasm from "unstable_wasm";
+import { TokenizerWasm } from "hf-tokenizers-wasm";
 
-console.log(wasm.tokenize("ab"));
-console.log(wasm.tokenize("abc"));
+class Tokenizer {
+  constructor(json) {
+    this.tokenizer = new TokenizerWasm(json);
+  }
+
+  static from_pretrained(name) {
+    return fetch(`https://huggingface.co/${name}/resolve/main/tokenizer.json`)
+      .then(response => response.text())
+      .then(json => new Tokenizer(json));
+  }
+
+  encode(text) {
+    return this.tokenizer.encode(text);
+  }
+}
+
+async function main() {
+    let tokenizer = await Tokenizer.from_pretrained("gpt2");
+    let encoding = tokenizer.encode("I love AI and privacy", false);
+    console.log(encoding.input_ids);
+    console.log(document);
+    document.tokenizer = tokenizer;
+}
+
+main();
